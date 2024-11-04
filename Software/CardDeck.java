9e4292ddc90d7732d7295c018249c25710616631
surajnagpal <150961823+surajnagpal@users.noreply.github.com>
2024-11-04 14:25:38 +0000

    Empty Classes push

--- Software/CardDeck.java
+++ Software/CardDeck.java
@@ -1,5 +1,63 @@
 package Software;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CardDeck {
+    private final int deckNumber;
+    private final LinkedList<Card> cards = new LinkedList<>();
+    private final ReentrantLock lock = new ReentrantLock();
     
+    public CardDeck(int deckNumber) {
+        this.deckNumber = deckNumber;
+    }
+    
+    public void addCard(Card card) {
+        lock.lock();
+        try {
+            cards.addLast(card);
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public Card drawCard() {
+        lock.lock();
+        try {
+            if (cards.isEmpty()) {
+                return null;
+            }
+            return cards.removeFirst();
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public List<Card> getCards() {
+        lock.lock();
+        try {
+            return new ArrayList<>(cards);
+        } finally {
+            lock.unlock();
+        }
+    }
+    
+    public int getDeckNumber() {
+        return deckNumber;
+    }
+    
+    @Override
+    public String toString() {
+        lock.lock();
+        try {
+            return "deck" + deckNumber + " contents: " + cards.stream()
+                .map(Card::toString)
+                .reduce((a, b) -> a + " " + b)
+                .orElse("");
+        } finally {
+            lock.unlock();
+        }
+    }
 }
--- Software/Player.java
+++ Software/Player.java
@@ -1,5 +1,1 @@
-package Software;
 
-public class Player {
-    
-}
