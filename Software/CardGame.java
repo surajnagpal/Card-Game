76d2b9f85bdc29eedd351f48880a74524bb32d59
jmasih1234 <120707221+jmasih1234@users.noreply.github.com>
2024-11-04 15:24:00 +0000

    updated Card Game

--- Software/CardGame.java
+++ Software/CardGame.java
@@ -1,1 +1,32 @@
+package Software;
+public class CardGame{
 
+    private List<Player> players;
+    private List<Deck> decks;
+    private List<Card> cardPack;
+    private int n;
+
+
+    public CardGame(int n, List<Card> cardPack){
+        this.n = n;
+    }
+
+    public void distributeHands(){
+        return;
+    }
+
+    public void startGame(){
+        return;
+    }
+
+    public void notifyWin(Player winner){
+        return;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+
+}
